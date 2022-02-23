7fd23b6fa7b79620a149188c935cd4839f5a9abe
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-23 15:38:12 -0500

    commit tree 9a5cd7c0ef1224311eccd752700fdf79d3af1fce

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
+    #setter
+    def name=(name)
+        @name = name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+
+    #getter
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
+
+wallE = Dog.new
+wallE.name = "WallE"
+wallE.breed = "American Black Lab"
+puts "#{wallE.name} is #{wallE.breed}"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
 
+    # setter methods
+    def name=(name)
+      @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+  
+    # getter methods
+    def name
+      @name
+    end
+
+    def job
+        @job
+    end
+  
 end
+
+kanye = Person.new
+kanye.name=("Kanye")
+puts kanye.name
